0e7aff1673476e6140000a80d00a5025698dd7d4
JohnnyWu2k <111260513+JohnnyWu2k@users.noreply.github.com>
2024-11-01 23:58:09 +0800

    add fuxing

---
+++ fuxing/data.js
@@ -0,0 +1,316 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "捷運忠孝復興站",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.03144466876740992,
+        "pitch": 0.22187173069238142,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4253760229744064,
+          "pitch": 0.06118744920409469,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": -0.10269141535356852,
+          "pitch": 0.153475916106256,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": 1.3072225565520448,
+          "pitch": 0.11409338905768607,
+          "rotation": 12.566370614359176,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.17756621832062436,
+          "pitch": -0.1717480469538959,
+          "title": "板南線",
+          "text": "<li><strong>範圍</strong>：頂埔站至南港展覽館站，橫跨台北、新北，總長28.3公里</li><li><strong>車站數</strong>：23站</li><li><strong>轉乘點</strong>：<ul><li>台北車站：連接機場捷運、台鐵、高鐵</li><li>忠孝復興站：與文湖線交會，SOGO百貨附近</li></ul></li><li><strong>特色站</strong>：西門站（西門町商圈）、南港展覽館站（展覽、商務中心）</li>"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "板南線",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.09204439071330306,
+          "pitch": 0.42128446918799867,
+          "rotation": 0,
+          "target": "5-"
+        },
+        {
+          "yaw": -0.3183434979151478,
+          "pitch": 0.4180380355613682,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": 3.0910750678225156,
+          "pitch": 0.5994397901806963,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-",
+      "name": "一號出口",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.1672259967861045,
+        "pitch": 0.039326796918157214,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.9507787139737482,
+          "pitch": 0.14077383916266406,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.8635561973924064,
+          "pitch": -0.07544766221365862,
+          "title": "一號出口",
+          "text": "<li><strong>購物</strong>：步行可達SOGO百貨、微風廣場</li><li><strong>美食</strong>：附近有多樣餐飲選擇，如莆田PUTIEN餐廳等</li>"
+        },
+        {
+          "yaw": 2.377474405827023,
+          "pitch": 0.09956579141584498,
+          "title": "安東街",
+          "text": "<li><strong>位置</strong>：台北市大安區，鄰近忠孝東路，步行即可到捷運忠孝復興站與SOGO百貨</li><li><strong>特色</strong>：<ul><li><strong>美食集中地</strong>：日式、韓式料理及特色甜點店眾多</li><li><strong>生活便利</strong>：便利商店、藥妝店和精品店林立</li><li><strong>文藝氛圍</strong>：巷弄安靜悠閒，充滿設計感</li></ul></li>"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "三號出口",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.8633888690959122,
+        "pitch": -0.033839751970564436,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.0120627064511254,
+          "pitch": 0.15090567716347536,
+          "rotation": 0,
+          "target": "5-"
+        },
+        {
+          "yaw": 1.328004189799545,
+          "pitch": 0.11899666226231531,
+          "rotation": 1.5707963267948966,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.2442140854830868,
+          "pitch": -0.4852429382302077,
+          "title": "遠東SOGO",
+          "text": "<li><strong>B2</strong>：超市與食品名店區，提供多樣生鮮食品與美食選擇。</li><li><strong>B1至10F</strong>：涵蓋時尚服飾、精品、美妝、家居用品等多元商品。</li><li><strong>11F</strong>：美食餐廳，匯集各國料理，滿足饕客需求。</li>"
+        }
+      ]
+    },
+    {
+      "id": "4-",
+      "name": "五號出口",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.4580603468071427,
+        "pitch": -0.016723527629114443,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2813467788108746,
+          "pitch": 0.25714057588794503,
+          "rotation": 5.497787143782138,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.0783068961643067,
+          "pitch": -0.48209759579838796,
+          "title": "SOGO",
+          "text": "<li><strong>購物</strong>：直接連通SOGO百貨復興館，該館以高檔精品品牌為主，提供多樣化的購物選擇。</li><li><strong>美食</strong>：館內設有多家知名餐廳和美食街，滿足不同口味需求。</li><li><strong>交通</strong>：位於忠孝東路與復興南路交叉口，交通便利，周邊有多條公車路線經過。</li>"
+        }
+      ]
+    },
+    {
+      "id": "5-",
+      "name": "四號出口",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.1549932365138424,
+        "pitch": -0.5119582766394579,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4433360912813455,
+          "pitch": 0.09519398832789605,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": 0.6693084110205731,
+          "pitch": 0.06365739805563564,
+          "rotation": 5.497787143782138,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.5752259341237682,
+          "pitch": -0.09719958379001525,
+          "title": "四號出口",
+          "text": "<li><strong>購物</strong>：緊鄰SOGO百貨，提供便利的購物選擇</li><li><strong>美食</strong>：附近有知名餐廳，如SOGO忠孝店12樓的米其林一星餐廳「莆田PUTIEN」，以及人氣早餐店「扶旺號復興店」，以創意三明治和蛋餅聞名</li>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};

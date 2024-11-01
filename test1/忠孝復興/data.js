fb93003e991d3c3403eeb8ba5fdbd1d228526477
JohnnyWu2k <111260513+JohnnyWu2k@users.noreply.github.com>
2024-11-01 11:24:15 +0800

    add test1(marzipano.zip)

---
+++ test1/忠孝復興/data.js
@@ -0,0 +1,152 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "一號出口",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.8203289053787524,
+        "pitch": 0.3354047744762898,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.9844111659594725,
+          "pitch": 0.12473390161519049,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.9645163342474268,
+          "pitch": -0.12448328091745786,
+          "title": "忠孝復興站",
+          "text": "一號出口"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "捷運忠孝復興站",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.9012005786984147,
+        "pitch": 0.1577189622664683,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.2957936752790893,
+          "pitch": 0.20483876318389704,
+          "rotation": 0.7853981633974483,
+          "target": "2-"
+        },
+        {
+          "yaw": -1.5050679920394625,
+          "pitch": 0.13118315325987773,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.5861407706875852,
+          "pitch": -0.19352340102182453,
+          "title": "大安路一段",
+          "text": "三號出口"
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "各號出口下面",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -3.0236408922820264,
+        "pitch": 0.3714057985235648,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1146747946946167,
+          "pitch": 0.4762715570080083,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 3.1240139158392726,
+          "pitch": -0.19209960673467386,
+          "title": "板南線",
+          "text": "經北車，市政府等等"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
